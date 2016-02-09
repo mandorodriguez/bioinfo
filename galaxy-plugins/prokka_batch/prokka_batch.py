8407b4922360f2e33d01e7ba3bd8816437f8742d
Mando Rodriguez <Armando.Rodriguez@utsa.edu>
2016-02-09 17:28:11 -0600

    1. added arguments to the prokka wrapper.

--- galaxy-plugins/prokka_batch/prokka_batch.py
+++ galaxy-plugins/prokka_batch/prokka_batch.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import argparse
+import pdb
 from subprocess import call
 
 
@@ -27,13 +28,33 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("arg_table", type=str,
                     help="The argument table to pass to prokka")
-parser.add_argument("-o", "--outfile", type=str,
-                    help="The output file", default="snp_genotype_out.txt")
-parser.add_argument("-t", "--translation_table", type=int, default=1,
-                    help="The translation table to use for amino translations")
-parser.add_argument("-g", "--groupfile", type=str,
-                    help="Output file indexed by pattern groups")
+parser.add_argument("--outdir", type=str)
+parser.add_argument("--prefix", type=str)
+parser.add_argument("--locustag", type=str)
+parser.add_argument("--increment", type=str)
+parser.add_argument("--gffver", type=str)
+parser.add_argument("--addgenes", type=str)
+parser.add_argument("--mincontig", type=str)
+parser.add_argument("--compliant", type=str)
+parser.add_argument("--centre", type=str)
+parser.add_argument("--genus", type=str)
+parser.add_argument("--species", type=str)
+parser.add_argument("--strain", type=str)
+parser.add_argument("--plasmid", type=str)
+parser.add_argument("--kingdom", type=str)
+parser.add_argument("--gcode", type=str)
+parser.add_argument("--usegenus", type=str)
+parser.add_argument("--proteins", type=str)
+parser.add_argument("--metagenome", type=str)
+parser.add_argument("--fast", type=str)
+parser.add_argument("--evalue", type=str)
+parser.add_argument("--rfam", type=str)
+parser.add_argument("--norrna", type=str)
+parser.add_argument("--notra", type=str)
 
+args = parser.parse_args()
+
+pdb.set_trace()
 
 
 
