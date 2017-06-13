dd95561ca9a7f3a4856502540aca2de8de678f39
Mando Rodriguez <Armando.Rodriguez@utsa.edu>
2017-06-13 16:18:15 -0500

    1. Added more parsing to the genbank flip script.

--- genbank_flip.py
+++ genbank_flip.py
@@ -33,6 +33,25 @@ def parse_genbank_file(genbank_file):
     return genbank_recs[0]
 
 #-------------------------------------------------------------------------------
+
+
+old_string = """
+     gene            %s..%s
+                     /locus_tag="%s"
+     CDS             %s..%s
+                     /locus_tag="%s"
+                     /product="Non-coding region"
+"""
+#-------------------------------------------------------------------------------
+
+gene_string = """
+      gene            %s..%s
+      /locus_tag="%s"
+"""
+
+
+
+#-------------------------------------------------------------------------------
 # Main function call
 def __main__():
 
@@ -46,13 +65,67 @@ def __main__():
 
     genbank_file = args.genbank_file
 
-    if args.outfile == "flipped.gb":
-        output_file = "flipped_%s" % genbank_file
+    output_file = args.outfile
     
     genbank_rec = parse_genbank_file(genbank_file)
 
     
+    source_start = genbank_rec.features[0].location.start
+    source_end = genbank_rec.features[0].location.end
 
+    gene_coords = []
+    
+    next_start = 0
+
+    for feature in genbank_rec.features[1:]:
+
+        if feature.type == "gene":
+            
+            this_start = int(feature.location.start)
+            this_end = int(feature.location.end)
+
+            if this_start > next_start:
+
+                gene_coords.append( (next_start,this_start) ) 
+
+                next_start = this_end-1
+        
+                
+    gene_coords.append( (next_start, int(source_end)) )
+        
+
+    # now we read in the genbank file and prepare to reoutput it.
+    genbank_lines = []
+    
+    with open(genbank_file, "rU") as input_handle:
+
+        for line in input_handle:
+            genbank_lines.append(line)
+
+    
+    # now we output the genbank
+    with open(output_file,"w") as output_handle:
+
+        for line in genbank_lines:
+            output_handle.write(line+"\n")
+
+            if "gene" in line:
+                break
+
+
+        for coords in gene_coords:
+            output_handle.write( gene_string % (coords[0],coords[1]) )
+
+        start_printing = False
+        for line in genbank_lines:
+
+            if "ORIGIN" in line:
+                start_printing = True
+
+            if start_printing:
+                output_handle.write(line+"\n")
+                
+    
     pdb.set_trace()
 
 
