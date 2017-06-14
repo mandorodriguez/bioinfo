a8be631e3395f0dbd9fac708025ac4cae940c625
Mando Rodriguez <Armando.Rodriguez@utsa.edu>
2017-06-14 15:50:22 -0500

    1. genbank_flip script now works.

--- genbank_flip.py
+++ genbank_flip.py
@@ -56,10 +56,14 @@ gene_string = """
 def __main__():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("genbank_file", type=str,
-                        help="The genbank file to flip", metavar="file.gb")
+    parser.add_argument("-p", "--prefix", type=str, 
+                        help="Prefix for the converted genes", default="NC", metavar="NC")
     parser.add_argument("-o", "--outfile", type=str,
                         help="The output file", default="flipped.gb",metavar="flipped_genbank.gb")
+    parser.add_argument("-m", "--min", type=str,
+                        help="Minimum Length to allow for a gene", default=0,metavar="MIN_LENGTH")
+    parser.add_argument("genbank_file", type=str,
+                        help="The genbank file to flip", metavar="file.gb")
 
     args = parser.parse_args()
 
@@ -69,7 +73,11 @@ def __main__():
     
     genbank_rec = parse_genbank_file(genbank_file)
 
+    gene_prefix = args.prefix
+
+    min_length = args.min
     
+    source = genbank_rec.features[0]
     source_start = genbank_rec.features[0].location.start
     source_end = genbank_rec.features[0].location.end
 
@@ -77,6 +85,7 @@ def __main__():
     
     next_start = 0
 
+    # collect what will be the new gene coords
     for feature in genbank_rec.features[1:]:
 
         if feature.type == "gene":
@@ -96,38 +105,49 @@ def __main__():
 
     # now we read in the genbank file and prepare to reoutput it.
     genbank_lines = []
-    
+    genbank_text = ""
     with open(genbank_file, "rU") as input_handle:
 
         for line in input_handle:
             genbank_lines.append(line)
 
-    
+    genbank_text = "".join(genbank_lines)
+
+    genbank_parts = genbank_text.split("FEATURES")
+
+    genbank_prefix = genbank_parts[0]
+
+    genbank_source_seq = genbank_parts[1].split("ORIGIN")[1]
+
+    nc_gene_num = 0
+
+
     # now we output the genbank
+
     with open(output_file,"w") as output_handle:
 
-        for line in genbank_lines:
-            output_handle.write(line+"\n")
+        output_handle.write(genbank_prefix)
 
-            if "gene" in line:
-                break
+        output_handle.write("FEATURES             Location/Qualifiers\n")
 
+        output_handle.write(format(' ',"5") + "source" + format(' ',"11") + "%s..%s" % (source_start+1,source_end+1) + "\n")
+
+        
+        for key,val in source.qualifiers.iteritems():
+            output_handle.write(format(' ',"22")+"/%s=\"%s\"\n" % (key, ",".join(val)))
 
         for coords in gene_coords:
-            output_handle.write( gene_string % (coords[0],coords[1]) )
+            
+            if coords[1]-coords[0] > min_length:
+                output_handle.write( gene_string % (coords[0]+1,coords[1]+1,"%s_%s" % (gene_prefix,format(nc_gene_num,"06"))) )
+                nc_gene_num += 1
 
-        start_printing = False
-        for line in genbank_lines:
-
-            if "ORIGIN" in line:
-                start_printing = True
-
-            if start_printing:
-                output_handle.write(line+"\n")
+        output_handle.write("ORIGIN\n")
+        output_handle.write(genbank_source_seq)
                 
     
-    pdb.set_trace()
 
-
+    print "Flipped genbank is written to %s" % output_file
+    
 #-------------------------------------------------------------------------------
 if __name__=="__main__": __main__()
