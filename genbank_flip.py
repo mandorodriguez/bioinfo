1748916a81f6e82ee34be74f225b428dbfe67b32
Mando Rodriguez <mandorodriguez@githib>
2020-10-15 14:06:42 -0500

    1. Added a verbose function to print out which coordinates are between genes.
    2. The coordinates now filter any points that are inside of an existing gene.

--- genbank_flip.py
+++ genbank_flip.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__version__="1.0"
+__version__="1.3"
 
 """
 Name: genbank_flip.py
@@ -21,7 +21,7 @@ def parse_genbank_file(genbank_file):
     
     genbank_recs = []
 
-    genbank_input_handle = open(genbank_file, "rU")
+    genbank_input_handle = open(genbank_file, "r")
     for gbrec in  SeqIO.parse(genbank_input_handle, "genbank"):
 
         genbank_recs.append(gbrec)
@@ -31,6 +31,58 @@ def parse_genbank_file(genbank_file):
     print("Read in %i genbank records from file %s" % (len(genbank_recs), genbank_file))
 
     return genbank_recs[0]
+
+#-------------------------------------------------------------------------------
+
+def in_gene(n,coords,verbose=False):
+
+
+    for c in coords:
+
+        # if n[0] >= c[0] and n[0] <= c[1]:
+
+        #     print("first coordinate in {} < {} < {}".format(c[0],n[0],c[1]))
+        #     return True
+
+
+        # if n[1] >= c[0] and n[1] <= c[1]:
+
+        #     print("second coordinate in {} < {} < {}".format(c[0],n[1],c[1]))
+        #     return True
+
+
+        if (n[0] >= c[0] and n[0] <= c[1]) or (n[1] >= c[0] and n[1] <= c[1]):
+
+            if verbose:
+                print_coord(n,c)
+
+            return True
+
+    
+    return False
+
+#-------------------------------------------------------------------------------
+
+def print_coord(n,c):
+
+
+    if n[0] == c[0]:
+        print("First position {},{} is equal to starting coord {},{}".format(n[0],n[1],c[0],c[1]))
+    elif n[1] == c[0]:
+        print("Second position {},{} is equal to starting coord {},{}".format(n[0],n[1],c[0],c[1]))
+    elif n[0] == c[1]:
+        print("First position {},{} is equal to ending coord {},{}".format(n[0],n[1],c[0],c[1]))
+    elif n[1] == c[1]:
+        print("Second position {},{} is equal to ending coord {},{}".format(n[0],n[1],c[0],c[1]))
+    elif (n[0] > c[0] and n[0] < c[1]) and (n[1] > c[0] and n[1] < c[1]):
+        print("Both positions {},{} are between coords {},{}".format(n[0],n[1],c[0],c[1]))
+    elif n[0] > c[0] and n[0] < c[1]:
+        print("First position {},{} is between coords {},{}".format(n[0],n[1],c[0],c[1]))
+    elif n[1] > c[0] and n[1] < c[1]:
+        print("Second position {},{} is between coords {},{}".format(n[0],n[1],c[0],c[1]))
+    else:
+        print("Coords aren't intergenic {} - {},{} - {}".format(n[0],c[0],c[1],n[1]))
+
 
 #-------------------------------------------------------------------------------
 
@@ -58,6 +110,8 @@ def __main__():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--prefix", type=str, 
                         help="Prefix for the converted genes", default="NC", metavar="NC")
+    parser.add_argument("-v", "--verbose", action="store_true", 
+                        help="Provides more output to the screen")
     parser.add_argument("-o", "--outfile", type=str,
                         help="The output file", default="flipped.gb",metavar="flipped_genbank.gb")
     parser.add_argument("-m", "--min", type=int,
@@ -75,14 +129,18 @@ def __main__():
 
     gene_prefix = args.prefix
 
+    verbose = args.verbose
+
     min_length = int(args.min)
     
     source = genbank_rec.features[0]
     source_start = genbank_rec.features[0].location.start
     source_end = genbank_rec.features[0].location.end
 
+    noncoding_coords = []
     gene_coords = []
-    
+    new_coords = []
+
     next_start = 0
 
     # collect what will be the new gene coords
@@ -93,20 +151,33 @@ def __main__():
             this_start = int(feature.location.start)
             this_end = int(feature.location.end)
 
+            gene_coords.append( (this_start, this_end) )
+
             if this_start > next_start:
 
-                gene_coords.append( (next_start,this_start) ) 
+                noncoding_coords.append( (next_start,this_start-1) ) 
 
-                next_start = this_end-1
+                next_start = this_end+1
         
-                
-    gene_coords.append( (next_start, int(source_end)) )
+
+    noncoding_coords.append( (next_start, int(source_end)) )
         
+
+    # filter out any overlaps here
+
+    for ncc in noncoding_coords:
+
+        if not in_gene(ncc,gene_coords,verbose):
+            new_coords.append(ncc)
+        else:
+            pass
+            #print("Removing overlap positions: {},{} with length {}".format(ncc[0],ncc[1],int(ncc[1] - ncc[0])))
+
 
     # now we read in the genbank file and prepare to reoutput it.
     genbank_lines = []
     genbank_text = ""
-    with open(genbank_file, "rU") as input_handle:
+    with open(genbank_file, "r") as input_handle:
 
         for line in input_handle:
             genbank_lines.append(line)
@@ -131,12 +202,11 @@ def __main__():
         output_handle.write("FEATURES             Location/Qualifiers\n")
 
         output_handle.write(format(' ',"5") + "source" + format(' ',"11") + "%s..%s" % (source_start+1,source_end+1) + "\n")
-
         
         for key,val in source.qualifiers.items():
             output_handle.write(format(' ',"22")+"/%s=\"%s\"\n" % (key, ",".join(val)))
 
-        for coords in gene_coords:
+        for coords in new_coords:
             
             if coords[1]-coords[0] > min_length:
                 output_handle.write( gene_string % (coords[0]+1,coords[1]+1,"%s_%s" % (gene_prefix,format(nc_gene_num,"06"))) )
